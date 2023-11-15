44b9ae69b1337dc6568c86692caca803efdaea7f
lynnlars <141281795+lynnlars@users.noreply.github.com>
2023-11-14 22:07:53 -0500

    Add files via upload
    .reduce method practice

---
+++ reduce-array/index.js
@@ -0,0 +1,83 @@
+// const arr = [1, 2, 3]
+
+// const result1 = arr.reduce(function(final, num){
+   // final += num
+ //   return final
+ //})
+
+ //console.log(result1)
+
+
+
+//  function stringConcat(arr) {
+//     // your code here
+//  }
+ 
+// const arr = [1, 2, 3]
+
+// const result = arr.reduce(function(a, b){
+//    return a + b
+// }, '')
+
+// console.log(result)
+
+ 
+ 
+
+//  const voters = [
+//     {name:'Bob' , age: 30, voted: true},
+//     {name:'Jake' , age: 32, voted: true},
+//     {name:'Kate' , age: 25, voted: false},
+//     {name:'Sam' , age: 20, voted: false},
+//     {name:'Phil' , age: 21, voted: true},
+//     {name:'Ed' , age:55, voted:true},
+//     {name:'Tami' , age: 54, voted:true},
+//     {name: 'Mary', age: 31, voted: false},
+//     {name: 'Becky', age: 43, voted: false},
+//     {name: 'Joey', age: 41, voted: true},
+//     {name: 'Jeff', age: 30, voted: true},
+//     {name: 'Zack', age: 19, voted: false}
+// ]
+
+// const voteCount = voters.reduce(function(final, voter){
+//    if(voter.voted){
+//        final++
+//    }
+//    return final
+// }, 0)
+
+// console.log(voteCount)
+
+
+
+ 
+// const wishlist = [
+//     { title: "Tesla Model S", price: 90000 },
+//     { title: "4 carat diamond ring", price: 45000 },
+//     { title: "Fancy hacky Sack", price: 5 },
+//     { title: "Gold fidgit spinner", price: 2000 },
+//     { title: "A second Tesla Model S", price: 90000 }
+// ]
+
+
+// const cost = wishlist.reduce(function(a , b){
+//   return a += b.price
+// }, 0)
+
+// console.log(cost)
+ 
+
+ 
+// var arrays = [
+//     ["1", "2", "3"],
+//     [true],
+//     [4, 5, 6]
+// ]
+
+// const combine = arrays.reduce(function(a, b){
+//    return a.concat(b)
+// })
+
+// console.log(combine)
+
+
